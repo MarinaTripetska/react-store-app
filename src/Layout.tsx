78af7affeedbc6837587fd4527e0035259c58e97
Maryna Tripetska <marina.tripetskaya@gmail.com>
2026-01-05 01:54:05 +0100

    refactor: inline totalQuantity in Cart component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,21 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Navbar from "@/components/Navbar.tsx";
 import Cart from "@/components/Cart.tsx";
-import { useCart } from "@/context/useCart";
 
 function Layout() {
-  const { cartItems } = useCart();
-
-  const totalQuantity = cartItems.reduce(
-    (sum, item) => sum + item.quantity,
-    0
-  );
-
   return (
     <>
       <header className="wrapper flex justify-between">
         <Navbar/>
-        <Cart value={totalQuantity}/>
+        <Cart/>
       </header>
 
       <Outlet/>
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,14 +1,19 @@
 import CartIcon from "@/assets/icons/CartIcon.tsx";
+import {useCart} from "@/context/useCart.ts";
 
-interface Props {
-  value: number;
-}
+function Cart() {
+  const {cartItems} = useCart();
 
-function Cart({ value }: Props) {
+  const totalQuantity = cartItems.reduce(
+    (sum, item) => sum + item.quantity,
+    0
+  )
+
   return (
     <div className="relative">
       <CartIcon/>
-      <div className="absolute -top-1/4 -right-1/4 min-w-6 w-fit h-6 p-1 rounded-full flex items-center justify-center bg-dark-100 opacity-90 text-xs text-white">{ value }</div>
+      <div
+        className="absolute -top-1/4 -right-1/4 min-w-6 w-fit h-6 p-1 rounded-full flex items-center justify-center bg-dark-100 opacity-90 text-xs text-white">{totalQuantity}</div>
     </div>
   )
 }
